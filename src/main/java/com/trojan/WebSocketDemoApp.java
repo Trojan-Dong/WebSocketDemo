ac1ee917f7c2c98e3048904b10d6adfc1d3b64d7
guoj.dong <1476154535@qq.com>
2023-09-21 17:28:58 +0800

    build project

--- src/main/java/com/trojan/WebSocketDemoApp.java
+++ src/main/java/com/trojan/WebSocketDemoApp.java
@@ -1,8 +1,14 @@
 package com.trojan;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.trojan.mapper")
 public class WebSocketDemoApp {
     
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(WebSocketDemoApp.class,args);
     }
 }
